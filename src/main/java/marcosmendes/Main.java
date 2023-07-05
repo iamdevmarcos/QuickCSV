f3b1ffb010694ac558e575b510577882ce9e74ee
Marcos Mendes <marcosdev.me@gmail.com>
2023-07-05 14:48:58 -0300

    feat: implements read and write csv with buffer

--- src/main/java/marcosmendes/Main.java
+++ src/main/java/marcosmendes/Main.java
@@ -1,19 +1,63 @@
 package marcosmendes;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import marcosmendes.entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        List<Product> productsList = new ArrayList<>();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        System.out.println("Enter source file path: ");
+        String sourceFilePath = scanner.nextLine();
+
+        File sourceFile = new File(sourceFilePath);
+        String sourceFolderPath = sourceFile.getParent();
+
+        new File(sourceFolderPath + "/out");
+        String outputFilePath = sourceFolderPath + "/out/summary.csv";
+
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(sourceFilePath))) {
+            String csvItem = bufferReader.readLine();
+            while (csvItem != null) {
+                String[] fields = csvItem.split(",");
+
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                productsList.add(new Product(name, price, quantity));
+                csvItem = bufferReader.readLine();
+            }
+
+            generateOutputFile(outputFilePath, productsList);
+        } catch(IOException error) {
+            System.out.println("Error writing file: " + error.getMessage());
+        }
+
+        scanner.close();
+    }
+
+    private static void generateOutputFile(String outputFilePath, List<Product> productsList) {
+        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(outputFilePath))) {
+            for (Product item: productsList) {
+                String productName = item.getName();
+                double totalPrice = item.totalPrice();
+
+                bufferWriter.write(productName + "," + totalPrice);
+                bufferWriter.newLine();
+            }
+
+            System.out.println(outputFilePath + " has been created with success!");
+        } catch(IOException error) {
+            System.out.println("Error writing file: " + error.getMessage());
         }
     }
 }
