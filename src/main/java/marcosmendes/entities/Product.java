eed8208cce39aeec06fd63093bab8536fa385efa
Marcos Mendes <marcosdev.me@gmail.com>
2023-07-05 14:47:25 -0300

    feat: implements product entity

---
+++ src/main/java/marcosmendes/entities/Product.java
@@ -0,0 +1,41 @@
+package marcosmendes.entities;
+
+public class Product {
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public double totalPrice() {
+        return price * quantity;
+    }
+}
